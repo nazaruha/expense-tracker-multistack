d93c88055dc2df269aeb63cedb9c5c40c4545592
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2025-12-15 15:08:09 +0100

    chore(frontend): set up alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+import * as path from 'node:path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react()],
 });
